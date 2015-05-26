1863606cc4fcd35b7334d48d6b570e284933c89f
Joseph Kobrosky <jkobrosky@gmail.com>
2015-05-26 13:50:40 -0600

    Completed all problems

--- practice.js
+++ practice.js
@@ -1,19 +1,23 @@
 //We're in a job interview. Answer the following questions (try to not look at your notes unless you have to).
   // 1) What is the purpose of the 'this keyword'?
 
-      //Answer
+      // Makes your funcitons more dynamic
 
   // 2) What are the four rules that govern what the 'this keyword' is bound to and describe each?
 
-      //Answer
+      // Implicit Binding - to the left of the dot at call time.
+      // Explicity Binding - .call or .apply || .bind - allows user to specify binding
+      // Default Binding - if .call, .apply or .bind are not used and Implicity binding is not used then it defaults to the window object
+      // New Binding - unk
 
   // 3) What is the difference between call and apply?
 
-      //Answer
+      // .call - allows you to pass in parameters one at a time separated by commas.
+      // .apply - allows you pass the parameters in an array
 
   // 4) What does .bind do?
 
-      //Answer
+      // .bind - returns a function that references the this binding
 
 
 //Next Problem
@@ -23,27 +27,45 @@
   //email --> which is a string
   //getUsername --> which is a function that returns the current object's username property. *Don't use 'user' instead use the 'this' keyword*
 
-    //Code Here
+var user = {
+  username: 'awesomeness',
+  email: 'awesomeness@gmail.com',
+  getUsername: function() {
+    return this.username;
+  }
+};
 
 //Now, invoke the getUsername method and verify you got the username of the object and not anything else.
 
 
-//Next Problem
+user.getUsername();
 
 
 // Write the function definitions which will make the following function invocations function properly.
 
-  //Function Invocations Here
+var Car = function(make, model, year) {
+  return {
+    make: make,
+    model: model,
+    year: year,
+    location: 0,
+    moveCar: moveCar
+  };
+}
+
 
 var prius = Car('Toyota', 'Prius', 2011);
 var mustang = Car('Ford', 'Mustang', 2013);
 
-prius.move(); //increments prius' move property by 10. Returns the new move property.
-mustang.move(); //increments prius' move property by 10. Returns the new move property.
+prius.moveCar(); //increments prius' move property by 10. Returns the new move property.
+mustang.moveCar(); //increments prius' move property by 10. Returns the new move property.
 
-//Hint, you'll need to write a move function which is added to every object that is being returned from the Car function. You'll also need to use the 'this' keyword properly in order to make sure you're invoking move on the write object (prius vs mustang).
+//Hint, you'll need to write a moveCar function which is added to every object that is being returned from the Car function. You'll also need to use the 'this' keyword properly in order to make sure you're invoking move on the write object (prius vs mustang).
 
-
+var moveCar = function() {
+  this.location += 10;
+  return this.location;
+}
 
 //Continuation of previous problem
 
@@ -53,9 +75,8 @@ var getYear = function(){
 
 //Above you're given the getYear function. Using your prius and mustang objects from above, use the proper syntax that will allow for you to call the getYear function with the prius then the mustang objects being the focal objects. *Don't add getYear as a property on both objects*.
 
-  //Code Here
-
-
+getYear.call(prius);
+getyear.call(mustang);
 
 //New Problem
 
@@ -68,17 +89,18 @@ var user = {
 };
 
 var getUsername = function(){
-  return this.username;
+  console.log(this.username);
 };
 
-setTimeout(getUsername, 5000);
+setTimeout(getUsername.bind(user), 5000);
 
 //Above you're given an object, a function, and a setTimeout invocation. After 5 seconds, what will the getUsername function return?
 
-  //Answer Here
+  // windows username
 
 //In the example above, what is the 'this keyword' bound to when getUsername runs?
 
-  //Answer Here
+  //default - window
 
 //Fix the setTimeout invocation so that the user object will be the focal object when getUsername is ran.
+
